6fc559b98e3f389fc015b90d9197d137a294536e
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-03 13:03:42 -0700

    Update Donations.js

--- components/Dashboard/Donations.js
+++ components/Dashboard/Donations.js
@@ -1,1 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
 
+const Donations = () => {
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/donations')
+      .then(response => setDonations(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <View>
+      <Text>Political Donations</Text>
+      {donations.map(donation => (
+        <Text key={donation.id}>{donation.amount} - {donation.donor}</Text>
+      ))}
+    </View>
+  );
+};
+
+export default Donations;
