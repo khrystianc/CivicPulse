489bdec41d2e435850f5adc640249c92c79923e6
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-03 13:05:32 -0700

    Update db.py

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,1 +1,11 @@
+from flask_mongoengine import MongoEngine
 
+db = MongoEngine()
+
+def initialize_db(app):
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'civicpulse',
+        'host': 'localhost',
+        'port': 27017
+    }
+    db.init_app(app)
