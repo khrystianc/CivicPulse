1da17a9fab201441aab454090abdb0277a6d4ca6
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-03 13:04:30 -0700

    Update donations_routes.py

--- backend/routes/donations_routes.py
+++ backend/routes/donations_routes.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, request, jsonify
+from controllers.donations_controller import get_donations
 
+donations_bp = Blueprint('donations_bp', __name__)
+
+@donations_bp.route('/', methods=['GET'])
+def fetch_donations():
+    return jsonify(get_donations())
