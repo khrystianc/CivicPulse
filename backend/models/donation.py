f3c2211efae58a6ea8a77563015929d91a248734
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-03 13:05:17 -0700

    Update donation.py

--- backend/models/donation.py
+++ backend/models/donation.py
@@ -1,1 +1,13 @@
+from mongoengine import Document, StringField, FloatField, DateTimeField
 
+class Donation(Document):
+    amount = FloatField(required=True)
+    donor = StringField(required=True)
+    date = DateTimeField(required=True)
+
+    def to_json(self):
+        return {
+            "amount": self.amount,
+            "donor": self.donor,
+            "date": self.date.isoformat()
+        }
